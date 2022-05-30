df62c30225474354c03f75413c656b188bb74655
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-05-30 23:28:58 +0200

    Coder show

--- app/controllers/coders_controller.rb
+++ app/controllers/coders_controller.rb
@@ -10,5 +10,12 @@ class CodersController < ApplicationController
                   .with_commit_stats
                   .with_repository_count
                   .find_by(github_name: params[:id])
+
+    @sort_column = params[:order_by]
+    @sort_column = 'score' unless %w[score commit_count additions deletions].include?(@sort_column)
+    @repositories = Repository.extending(CommitStats)
+                              .with_commit_stats
+                              .where(commits: { coder_id: @coder.id })
+                              .order({ @sort_column => :desc })
   end
 end
