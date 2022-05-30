0aa889f1b3e4b59e9d87ee23e456744c83db828b
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-05-30 22:28:14 +0200

    Fix webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -6,11 +6,11 @@ class WebhookController < ApplicationController
     return head :bad_request if event.blank?
     return head :ok unless event == 'push'
 
-    repo_data = data['repository']
+    repo_data = request.params['repository']
     return head :ok unless repo_data['owner']['login'] == Rails.application.config.organisation
     return head :ok if repo_data['private']
 
-    Repository.create_or_update_from_github_api(name: repo_data['name'], github_url: repo_data['html_url'], clone_url: repo_data['clone_url'])
+    Repository.create_or_update_from_github_api(repo_data['name'], repo_data['html_url'], repo_data['clone_url'])
 
     head :ok
   end
