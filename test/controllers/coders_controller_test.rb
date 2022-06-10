d5274e4bba686b1d9f7b1d4f6c0235dda6c4e355
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-06-10 21:21:20 +0200

    Fix coder controller tests

--- test/controllers/coders_controller_test.rb
+++ test/controllers/coders_controller_test.rb
@@ -10,39 +10,8 @@ class CodersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get new' do
-    get new_coder_url
-    assert_response :success
-  end
-
-  test 'should create coder' do
-    assert_difference('Coder.count') do
-      post coders_url, params: { coder: { avatar_url: @coder.avatar_url, full_name: @coder.full_name, github_name: @coder.github_name, github_url: @coder.github_url } }
-    end
-
-    assert_redirected_to coder_url(Coder.last)
-  end
-
   test 'should show coder' do
-    get coder_url(@coder)
+    get coder_url(@coder.github_name)
     assert_response :success
-  end
-
-  test 'should get edit' do
-    get edit_coder_url(@coder)
-    assert_response :success
-  end
-
-  test 'should update coder' do
-    patch coder_url(@coder), params: { coder: { avatar_url: @coder.avatar_url, full_name: @coder.full_name, github_name: @coder.github_name, github_url: @coder.github_url } }
-    assert_redirected_to coder_url(@coder)
-  end
-
-  test 'should destroy coder' do
-    assert_difference('Coder.count', -1) do
-      delete coder_url(@coder)
-    end
-
-    assert_redirected_to coders_url
   end
 end
