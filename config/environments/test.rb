aed71b0654777e17592d8ce896b5d47e14605283
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-11-09 11:34:43 +0100

    Update to rails 8

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,11 +18,10 @@ Rails.application.configure do
   config.eager_load = ENV['CI'].present?
 
   # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{1.hour.to_i}" }
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
-  # Show full error reports and disable caching.
+  # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
